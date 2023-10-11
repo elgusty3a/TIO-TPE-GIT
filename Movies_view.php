d6b68a9e596cf4e48e942fed2694d51c5726aaa5
Agustin del Canto <agustindelcantod@gmail.com>
2023-10-11 19:53:06 -0300

    agrego archivo de vista para MVC

---
+++ Movies_view.php
@@ -0,0 +1,11 @@
+<?php
+class moviesView
+{
+
+    public function showMovies($movies, $param)
+    {
+        echo "<h1>Lista por género: $param</h2>";
+        echo "<a href='router.php'> Volver </a>";
+    }
+
+    ?>
