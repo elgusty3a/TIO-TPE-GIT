cb59664ddba2dd9c120515679919776410182879
nahuel. <seguelnahuel@gmail.com>
2023-10-11 18:01:48 -0300

    creadas funciones get por genero y estudio.

--- movies_model.php
+++ movies_model.php
@@ -9,8 +9,21 @@ class moviesModel
         $this->pdo = new PDO('mysql:host=localhost;' . 'dbname=db_movies;charset=utf8', 'root', '');
     }
 
-}
+    public function getMoviesByGenre($genre)
+    {
+        $db = $this->pdo;
+        $query = $db->prepare('SELECT * FROM movies WHERE genre = ?');
+        $query->execute([$genre]);
+        $movies = $query->fetchAll(PDO::FETCH_OBJ);
+        return $movies;
+    }
+    public function getMoviesByStudio($studio)
+    {
+        $db = $this->pdo;
+        $query = $db->prepare('SELECT * FROM movies WHERE studio = ?');
+        $query->execute([$studio]);
+        $movies = $query->fetchAll(PDO::FETCH_OBJ);
+        return $movies;
+    }
 
-
- ?>
-
+?>
