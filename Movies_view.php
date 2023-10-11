f5de6747ed26e3458488d87cd2d9f0d7e950928f
Agustin del Canto <agustindelcantod@gmail.com>
2023-10-11 19:56:38 -0300

    agregue funcion a vista

--- Movies_view.php
+++ Movies_view.php
@@ -6,6 +6,23 @@ class moviesView
     {
         echo "<h1>Lista por género: $param</h2>";
         echo "<a href='router.php'> Volver </a>";
-    }
-
-    ?>
+  // imprime la tabla de peliculas
+  echo "<table>
+  <thead>
+      <tr>
+          <th>Título</th>
+          <th>Año</th>
+          <th>Estudio</th>
+      </tr>
+  <thead>
+  <tbody>";
+foreach ($movies as $movie) {
+echo "  <tr>
+          <td>$movie->title</td>
+          <td>$movie->year</td>
+          <td>$movie->studio</td>
+      </tr>";
+}
+echo "  </tbody>    
+</table>";
+}
