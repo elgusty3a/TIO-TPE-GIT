51091fb4e1f3b448417f4b8352b1faff5618d850
Gustavo Arias <ariasgustavo3a@gmail.com>
2023-10-12 00:57:55 -0300

    refactor para MVC y se hizo el archivo movies_controller.php

---
+++ app/controllers/movies_controller.php
@@ -0,0 +1,32 @@
+<?php
+require_once('./app/views/movies_view.php');
+require_once('./app/models/movies_model.php');
+
+class moviesController
+{
+    private $model;
+    private $view;
+
+    public function __construct()
+    {
+        $this->model = new moviesModel;
+        $this->view = new moviesView;
+    }
+
+    public function showMoviesByGenre(){
+        $genre=$this->model->getParam('genre');
+        $movies=$this->model->getMoviesByGenre($genre);
+        $this->view->showMovies($movies,$genre);
+    }
+
+    public function showMoviesByStudio(){
+        $studio=$this->model->getParam('studio');
+        $movies=$this->model->getMoviesByStudio($studio);
+        $this->view->showMovies($movies,$studio);
+    }
+
+    public function showIndex(){
+        $this->view->showIndex();
+    }
+}
+?>
