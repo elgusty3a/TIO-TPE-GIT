7efe1b23805d3f460ea3a78b2033aa72b39657a6
Franco Di cheronimo <d1ch33@hotmail.com>
2023-10-14 11:26:48 -0300

    Se aplico una segunda condicion, o un 'O SINO ( elseif )

--- app/router/router.php
+++ app/router/router.php
@@ -11,5 +11,10 @@ if (isset($_GET['studio'])&&!empty(isset($_GET['studio']))) {
 // Linea 8 : preguntamos si el parametro 'studio' esta presente en la url, y si no esta vacio 
 // Linea 9 : si entra al If : se llama al metodo 'showMoviesByStudio', de la instancia moviesController (Hace algo).
 
+else if (isset($_GET['genre'])&&!empty(isset($_GET['genre']))) {
+    $MoviesController->showMoviesByGenre();
+} 
+// Linea 14 y 15, misma funcionalidad que linea 8 Y 9, pero con diferente parametro : 'genre'.
+
 
 ?>
