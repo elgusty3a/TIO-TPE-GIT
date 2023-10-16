ac623e1a39cdb4b9dedbc73d0ca7a770330ed0af
Franco Di cheronimo <d1ch33@hotmail.com>
2023-10-16 19:40:37 -0300

    se eliminaron los comentarios en el codigo

--- router.php
+++ router.php
@@ -2,24 +2,17 @@
 
 require_once('movies_controller.php');
 $MoviesController = new moviesController;
-// Linea 3 : estamos incluyendo al archivo "movies_controller.php" en este script.
-// Linea 4 : Instanciamos moviesController, y creamos una variable para almanece sus metodos.
 
 if (isset($_GET['studio'])&&!empty(isset($_GET['studio']))) {
     $MoviesController->showMoviesByStudio();
 } 
-// Linea 8 : preguntamos si el parametro 'studio' esta presente en la url, y si no esta vacio 
-// Linea 9 : si entra al If : se llama al metodo 'showMoviesByStudio', de la instancia moviesController (Hace algo).
 
 else if (isset($_GET['genre'])&&!empty(isset($_GET['genre']))) {
     $MoviesController->showMoviesByGenre();
 } 
-// Linea 14 y 15, misma funcionalidad que linea 8 Y 9, pero con diferente parametro : 'genre'.
 
 else {
     $MoviesController->showIndex();
 }
-
-// Linea 20: se llama al metodo showIndex de la instancia moviesController, para llevarnos a la pagina de inicio 
 
 ?>
